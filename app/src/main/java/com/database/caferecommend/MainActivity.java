d786ec5d7675cdf8f1dc50314dd3ace191875724
unknown <>
2016-11-25 20:29:21 +0900

    up

--- app/src/main/java/com/database/caferecommend/MainActivity.java
+++ app/src/main/java/com/database/caferecommend/MainActivity.java
@@ -2,6 +2,8 @@ package com.database.caferecommend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spin = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.content, android.R.layout.simple_spinner_item);
     }
 }
