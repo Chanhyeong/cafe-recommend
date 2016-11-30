82ac8f845f9f2d099afe2a35849a70e0b644ce52
unknown <>
2016-11-30 21:11:31 +0900

    dd

--- app/src/main/java/com/database/caferecommend/SubActivity.java
+++ app/src/main/java/com/database/caferecommend/SubActivity.java
@@ -1,7 +1,11 @@
 package com.database.caferecommend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class SubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        findViewById(R.id.revBtn).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),Review.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
