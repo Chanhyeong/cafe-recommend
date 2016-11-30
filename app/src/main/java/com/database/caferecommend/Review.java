ff767ecee21ca28642b21c65a2157e2fee45b425
unknown <>
2016-11-30 21:41:35 +0900

    d

---
+++ app/src/main/java/com/database/caferecommend/Review.java
@@ -0,0 +1,18 @@
+package com.database.caferecommend;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016-11-30.
+ */
+
+public class Review extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.review_activity);
+
+        //
+    }
+}
