ccde7410edfa8c292fcaf73c60ac29627f55dce7
unknown <>
2016-12-01 14:52:39 +0900

    d

---
+++ app/src/main/java/com/database/caferecommend/registerActivity.java
@@ -0,0 +1,37 @@
+package com.database.caferecommend;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Created by Administrator on 2016-12-01.
+ */
+
+public class registerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_review);
+
+        findViewById(R.id.ratingBar2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        findViewById(R.id.avgBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        EditText editText=(EditText) findViewById(R.id.revEdit);
+        String review=editText.getText().toString();
+    }
+}
