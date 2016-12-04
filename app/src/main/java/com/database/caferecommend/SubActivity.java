0b6991cecb799c0631e73374c6c8d635b8f1ed91
unknown <>
2016-12-04 19:56:46 +0900

    1st

--- app/src/main/java/com/database/caferecommend/SubActivity.java
+++ app/src/main/java/com/database/caferecommend/SubActivity.java
@@ -1,7 +1,6 @@
 package com.database.caferecommend;
 
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -11,12 +10,6 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.DatabaseErrorHandler;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
-import android.database.sqlite.SQLiteOpenHelper;
 
 import org.json.JSONArray;
 import org.json.JSONException;
